3c0bfd6b0909c257a9b99b184069118ef2f66b54
Zachary PC <Zachary PC@ZACHARY>
2023-10-06 19:16:46 -0400

    Started working on the match() method by implementing the two foreach loops that are for now only separating each words from the two string arrays into characters

--- WordMatcher/WordMatcher.cs
+++ WordMatcher/WordMatcher.cs
@@ -8,9 +8,27 @@ namespace WordUnscrambler
 {
     class WordMatcher
     {
+        String word;
+        String scramble;
         public List<MatchedWord> Match(string[] scrambledWords, string[] wordList)
         {
             List<MatchedWord> matchedWords = new List<MatchedWord>();
+
+            foreach (string scramble in scrambledWords)
+            {
+                char[] CharScramble = scramble.ToCharArray();
+                //separating each words from the scramble array into characters 
+                //not done
+
+                foreach (string word in wordList)
+                {
+                    char[] CharWord = word.ToCharArray();
+
+                    //separating each words from the wordList array into characters 
+                    //not done
+
+                }
+            }
 
             // Implement code here.
             // Work with "scrambledWords" and "matchedWords".
